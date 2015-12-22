d682752abd6b7f39bce70ed979bf83fafb317e6a
aiddroid <aiddroid@gmal.com>
2015-12-22 17:12:47 +0800

    remove useless js

--- assets/admin.js
+++ assets/admin.js
@@ -7,11 +7,7 @@ function wp_admin_notice_setup_js() {
     //If it does exist then we initialize the WordPress color picker on our text input field
     if ( typeof jQuery.wp === 'object' && typeof jQuery.wp.wpColorPicker === 'function' ) {
         jQuery('#wp_admin_notice_options_text_color').wpColorPicker();
-        jQuery('#wp_admin_notice_options_text_bg_color').wpColorPicker();
-        jQuery('#wp_admin_notice_options_link_color').wpColorPicker();
     } else { //We use farbtastic if the WordPress color picker widget doesn't exist
         jQuery('.wp_admin_notice_admin_wrapper #text_color_picker').farbtastic('#wp_admin_notice_options_text_color');
-        jQuery('.wp_admin_notice_admin_wrapper #text_bg_color_picker').farbtastic('#wp_admin_notice_options_text_bg_color');
-        jQuery('.wp_admin_notice_admin_wrapper #link_color_picker').farbtastic('#wp_admin_notice_options_link_color');
     }
 }
